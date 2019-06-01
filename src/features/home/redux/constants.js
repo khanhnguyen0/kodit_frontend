b8190a555058eba007bffbb63f3813f0ef8f9a81
Khanh <khanh@vainu.io>
2019-06-01 13:28:30 +0300

    add DATA_SOURCE constant

--- src/features/home/redux/constants.js
+++ src/features/home/redux/constants.js
@@ -8,4 +8,10 @@ export const HOME_FETCH_CLUSTER_FAILURE = 'HOME_FETCH_CLUSTER_FAILURE';
 export const HOME_FETCH_CLUSTER_DISMISS_ERROR = 'HOME_FETCH_CLUSTER_DISMISS_ERROR';
 export const HOME_OPEN_BOTTOM_BAR = 'HOME_OPEN_BOTTOM_BAR';
 export const HOME_CLOSE_BOTTOM_BAR = 'HOME_CLOSE_BOTTOM_BAR';
-export const API = "https://serene-thicket-22556.herokuapp.com"
+export const API = 'https://serene-thicket-22556.herokuapp.com';
+export const HOME_SELECT_DISPLAY_DATA = 'HOME_SELECT_DISPLAY_DATA';
+export const DATA_SOURCE = [
+  { value: 'living_area_sqm', label: 'Living area' },
+  { value: 'price', label: 'Price' },
+  { value: 'price_sqm', label: 'Price per m²' },
+];
